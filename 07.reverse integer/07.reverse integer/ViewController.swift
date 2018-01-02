f0e42453bcbe4011c1bcceb5d3d75c090ce96c62
张冬冬 <zdd@microfeel.net>
2018-01-02 17:16:52 +0800

    07.reverse integer 第二版本, 跟第一版差不多, 只是提前退出

--- 07.reverse integer/07.reverse integer/ViewController.swift
+++ 07.reverse integer/07.reverse integer/ViewController.swift
@@ -12,24 +12,40 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let a = reverse(1234)
+        print(a)
     }
     
     func reverse(_ x: Int) -> Int {
-        var temp = x
+        var temp = abs(x)
         var num = 0
         
-        while temp != 0 {
-            num = num * 10 + temp % 10
+        while temp > 0 {
+            let next = num * 10
+            if next > Int32.max {
+                return 0
+            }
+            num = next + temp % 10
             temp /= 10
         }
-        
-        if num > Int32.max || num < Int32.min {
-            return 0
-        }
-        
-        return num
+        return num * ((x < 0) ? -1 : 1)
     }
+    
+//    func reverse(_ x: Int) -> Int {
+//        var temp = x
+//        var num = 0
+//
+//        while temp != 0 {
+//            num = num * 10 + temp % 10
+//            temp /= 10
+//        }
+//
+//        if num > Int32.max || num < Int32.min {
+//            return 0
+//        }
+//
+//        return num
+//    }
     
 }
 
