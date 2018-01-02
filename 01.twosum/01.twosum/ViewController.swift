dcb362bf49efc61322ca8ba0aefcddbbedfc0f97
张冬冬 <zdd@microfeel.net>
2018-01-02 16:18:50 +0800

    01.twosum 第二版本

--- 01.twosum/01.twosum/ViewController.swift
+++ 01.twosum/01.twosum/ViewController.swift
@@ -15,26 +15,26 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        for var i in 0 ..< nums.count {
-            for var j in i + 1 ..< nums.count {
-                if nums[i] + nums[j] == target {
-                    return [i, j];
-                }
-            }
-        }
-        fatalError("intput error!")
-    }
-
 //    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-//        var dict = [Int: Int]();
-//        for (i, num) in nums.enumerated() {
-//            if let last = dict[target - num] {
-//                return [last, i]
+//        for var i in 0 ..< nums.count {
+//            for var j in i + 1 ..< nums.count {
+//                if nums[i] + nums[j] == target {
+//                    return [i, j];
+//                }
 //            }
-//            dict[num] = i
 //        }
 //        fatalError("intput error!")
 //    }
+
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dict = [Int: Int]();
+        for (i, num) in nums.enumerated() {
+            if let last = dict[target - num] {
+                return [last, i]
+            }
+            dict[num] = i
+        }
+        fatalError("intput error!")
+    }
 }
 
